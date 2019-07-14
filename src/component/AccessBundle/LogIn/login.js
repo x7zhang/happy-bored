a16d9150dd31d64d614fc7a06cbe8e5d560c7377
x7zhang <x7zhang@gmail.com>
2019-07-13 22:48:22 -0400

    set up access bundle

--- src/component/AccessBundle/LogIn/login.js
+++ src/component/AccessBundle/LogIn/login.js
@@ -2,31 +2,105 @@
 //check if username + password matched on our history.
 //create authentication token, store in cookie/localstorage
 //authentication token to help long time login
-import React from 'react';
+
+//input change
+
+import React, { useState } from 'react';
 
 const LogIn = () => {
+    const [userName, setUserName] = useState('');
+    const [passWord, setPassWord] = useState('');
+    const [usernameValid, setUserNameValid] = useState(true);
+    const [passwordValid, setPassWordValid] = useState(true);
+    const [errorStyle, setErrorStyle] = useState({display:'none'});
+    const [userPassed, SetUserPassed] = useState(false);
+
+    const validation = (userInfo, name, password) => {
+      if (password === userInfo.password && name === userInfo.username) {
+        setErrorStyle({display:'none'})
+        return true;
+      }
+      if (password === userInfo.password) {
+        if (name !== userInfo.username) {
+          setUserNameValid(false);
+          setErrorStyle({display:'show'});
+          return false;
+        }
+      }
+
+      if (name === userInfo.username) {
+        if (password !== userInfo.password) {
+          setPassWordValid(false);
+          setErrorStyle({display:'show'});
+          return false;
+        }
+      }
+      
+    };
+    //logic problem, fix tomorrow
+    const getUserInfo = () => {
+      //simple way to check if info valid
+      const userData = JSON.parse(localStorage.getItem('signedData'));
+      for (let i = 0; i < userData.length; i++) {
+        const userInfo = JSON.parse(userData[i]);
+        const name = userName;
+        const password = passWord;
+
+        if (password === userInfo.password && name === userInfo.username) {
+          console.log('1111');
+          setErrorStyle({display:'none'});
+          SetUserPassed(true);
+          
+        }
+
+        if (password === userInfo.password) {
+          if (name !== userInfo.username) {
+            console.log('222');
+            setUserNameValid(false);
+            setErrorStyle({display:'show'});
+          }
+        }
+  
+        if (name === userInfo.username) {
+          if (password !== userInfo.password) {
+            console.log('333');
+            setPassWordValid(false);
+            setErrorStyle({display:'show'});
+          }
+        }
+        
+        //console.log('sadasd', item.username);
+      }
+      
+    };
     return (
-      <form className='left-label-form login-form' id='retrieve-password-form' action='#0'>
+      <div className='left-label-form login-form' id='retrieve-password-form' action='#0'>
             <input name='utf8' type='hidden' value='✓' />
             <input type='hidden' name='authenticity_token' value='fsafaxvdsfs1aegaad' />
             <div id='login-username' className='field'>
-                <label htmlFor='login-username-field'>UserName</label>
-                <input type='text' name='username' id='login-username-field' required='required' />
-                <div className='error-message'>The username or password you entered is incorrect, please try again.</div>
+                <label htmlFor='login-username-field'>UserName/Email</label>
+                <input type='text' name='username' value={userName} onChange={(e) => setUserName(e.target.value)} id='login-username-field' required='required' />
+                {
+                  usernameValid ? '' : <div className='error-message' style={errorStyle}>The Username Or Email You Entered is Incorrect, Please Try Again.</div>
+                }
             </div>
             <div id='login-password' className='field'>
                 <label htmlFor='login-password-filed'>PassWord</label>
-                <input type='text' name='password' id='login-password-field' required='required' />
+                <input type='password' name='password' onChange={(e) => setPassWord(e.target.value)} value={passWord} id='login-password-field' required='required' />
+                <div className='error-message' style={errorStyle}>The PassWord You Entered is Incorrect, Please Try Again.</div>
+                {
+                  passwordValid ? '' : <div className='error-message' style={errorStyle}>The PassWord You Entered is Incorrect, Please Try Again.</div>
+                }
             </div>
             <div id='login-error'></div>
             <div>
                 <label className='spacing-label'>&nbsp;</label>
-                <button id='log-in-button' className='recaptcha-trigger-button button green action-button expand-right'>
+                <button id='log-in-button' onClick={() => getUserInfo()} className='recaptcha-trigger-button button green action-button expand-right'>
                     <span className='label'>Log in</span>
                     <span className='spinner'></span>
                 </button>
             </div>
-    </form>
+    </div>
     );
 }
 
--- src/component/AccessBundle/SignUp/signup.js
+++ src/component/AccessBundle/SignUp/signup.js
@@ -4,35 +4,83 @@
 //email
 //profile pic?
 
-import React from 'react';
+//input change
+//email validation
+
+//store in localstorage to monitor database
+// we should encryp user info, but inorder to simplifier it
+// we just creat an simple encryption fun
+//data format:
+// {
+//    username: '',
+//    email: '',
+//    password: '',
+// }
+
+import React, { useState } from 'react';
 
 
 const SignUp = () => {
+    const [userName, setUserName] = useState('');
+    const [userEmail, setUserEmail] = useState('');
+    const [passWord, setPassWord] = useState('');
+    const [emailValid, setEmailValidation] = useState(true);
+    const [errorStyle, setErrorStyle] = useState({display:'none'});
+    
+    const validation = (email) => {
+        const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    };
+    const validateEmail = (e) => {
+        if (!validation(e.target.value)) {
+            setEmailValidation(false);
+            setErrorStyle({display: 'show'});
+        }
+        else {
+            setEmailValidation(true)
+            setErrorStyle({display: 'none'});
+        }
+    };
+
+    const SignUp = (e) => {
+        const userInfo = JSON.stringify({
+            username: userName,
+            email: userEmail,
+            password: passWord
+        });
+        const retrievedData = JSON.parse(localStorage.getItem("signedData") || "[]");
+        retrievedData.push(userInfo);
+        localStorage.setItem("signedData", JSON.stringify(retrievedData));
+
+    }
     return (
-        <form className="login-form right-label-form" id="login-login-form" action='/login' acceptCharset='UTF-8' >
+        <div className="login-form right-label-form" id="login-login-form" acceptCharset='UTF-8' >
             <div id='signup-username' className='field'>
                 <label htmlFor='signup-username-field'>UserName</label>
-                <input type='text' name='username' id='signup-username-field' required='required' />
-                <div className='error-message'>The username or password you entered is incorrect, please try again.</div>
+                <input type='text' name='username' value={userName} onChange={(e) => setUserName(e.target.value)} id='signup-username-field' required='required' />
             </div>
-            <div id='signip-email' className='field'>
+            <div id='signup-email' className='field'>
                 <label htmlFor='signup-email-filed'>Email</label>
-                <input type='text' name='email' id='signup-email-field' required='required' />
+                <input type='text' name='email' value={userEmail} onChange={(e) => setUserEmail(e.target.value)} onBlur={(e) => validateEmail(e)} id='signup-email-field' required='required' />
+                {
+                    emailValid ? '' : <div className='error-message' style={errorStyle}>Please Type Correct Format Of Email(xxx@host.com).</div>
+                }
+                
             </div>
-            <div id='signip-password' className='field'>
+            <div id='signup-password' className='field'>
                 <label htmlFor='signup-password-filed'>PassWord</label>
-                <input type='text' name='password' id='signup-password-field' required='required' />
+                <input type='text' name='password' value={passWord} onChange={(e) => setPassWord(e.target.value)} id='signup-password-field' required='required' />
             </div>
             <div id='signup-error'></div>
             <div>
                 <label className='spacing-label'>&nbsp;</label>
-                <button id='signup-in-button' className='recaptcha-trigger-button button green action-button expand-right'>
+                <button id='signup-in-button' onClick={() => SignUp()} className='recaptcha-trigger-button button green action-button expand-right'>
                     <span className='label'>Sign Up</span>
                     <span className='spinner'></span>
                 </button>
             </div>
 
-        </form>
+        </div>
     );
 };
 
